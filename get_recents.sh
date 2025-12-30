0f3a3040f65205467d2f8a0b541fa8c659048217
topchyan <topchyan@gmail.com>
2025-12-30 13:39:39 -0500

    chore: ignore local artifacts and remove tracked recents files

--- get_recents.sh
+++
@@ -1,4 +0,0 @@
-rm -f recents.zip
-{ git diff --name-only -z HEAD -- '*.md' '*.js' '*.mjs' '*.css' '*.html' '*.sh';
-  git ls-files -z --others --exclude-standard -- '*.md' '*.js' '*.mjs' '*.css' '*.html' '*.sh';
-} | xargs -0 zip -9 recents.zip
